adde64f7ab2a78963b303b56e5a2eb5bbc88f651
PedroYutaroUEA <pymmn.snf23@uea.edu.br>
2025-05-20 22:13:28 -0400

    prisam client db url

--- backend/src/api/prisma.ts
+++ backend/src/api/prisma.ts
@@ -10,7 +10,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: process.env.DATABASE_URL,
+      url: process.env.PROD_DATABASE_URL || "file:./prisma/prod.db",
     },
   },
 });
